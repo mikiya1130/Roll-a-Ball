d6aa808844461bca09c043cd455166d11b897161
Morisaki Mikiya <git@morisakimikiya.com>
2021-08-27 00:13:50 +0900

    Add replay

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,11 +6,15 @@ public class PlayerController : MonoBehaviour
 {
     public float speed = 10.0f;
 
+    public GameObject Item;
+
     Rigidbody rb;
 
     float inputHorizontal;
 
     float inputVertical;
+
+    int countItem;
 
     // Start is called before the first frame update
     void Start()
@@ -23,19 +27,28 @@ public class PlayerController : MonoBehaviour
     {
         inputHorizontal = Input.GetAxisRaw("Horizontal");
         inputVertical = Input.GetAxisRaw("Vertical");
+
+        countItem = GameObject.FindGameObjectsWithTag(Item.tag).Length;
     }
 
     void FixedUpdate()
     {
-        Vector3 forward =
-            Vector3
-                .Scale(Camera.main.transform.forward, new Vector3(1, 0, 1))
-                .normalized;
+        if (countItem != 0)
+        {
+            Vector3 forward =
+                Vector3
+                    .Scale(Camera.main.transform.forward, new Vector3(1, 0, 1))
+                    .normalized;
 
-        Vector3 move =
-            forward * inputVertical +
-            Camera.main.transform.right * inputHorizontal;
+            Vector3 move =
+                forward * inputVertical +
+                Camera.main.transform.right * inputHorizontal;
 
-        rb.AddForce(move * speed);
+            rb.AddForce(move * speed);
+        }
+        else
+        {
+            rb.velocity = Vector3.zero;
+        }
     }
 }
---
+++ Assets/Scripts/ReplayButton.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ReplayButton : MonoBehaviour
+{
+    public void OnClick()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
