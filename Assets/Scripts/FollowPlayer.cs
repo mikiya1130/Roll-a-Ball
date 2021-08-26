b8ffee821ae5b2375417fa958eb1de6927944522
Morisaki Mikiya <git@morisakimikiya.com>
2021-08-26 21:29:00 +0900

    Update camera randomly

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject Target;
+
+    Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        System.Random rand = new System.Random();
+        int rotate = 90 * rand.Next(4);
+        transform.RotateAround(Vector3.zero, Vector3.up, rotate);
+
+        offset = transform.position - Target.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Target.transform.position + offset;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 10.0f;
+
+    Rigidbody rb;
+
+    float inputHorizontal;
+
+    float inputVertical;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        inputHorizontal = Input.GetAxisRaw("Horizontal");
+        inputVertical = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 forward =
+            Vector3
+                .Scale(Camera.main.transform.forward, new Vector3(1, 0, 1))
+                .normalized;
+
+        Vector3 move =
+            forward * inputVertical +
+            Camera.main.transform.right * inputHorizontal;
+
+        rb.AddForce(move * speed);
+    }
+}
