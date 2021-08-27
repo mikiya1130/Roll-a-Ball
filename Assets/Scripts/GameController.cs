e169f34e8e832e8d1d7d113cb3806349968e263c
Morisaki Mikiya <git@morisakimikiya.com>
2021-08-28 00:49:20 +0900

    Change GameController macro to script

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject ScoreLabel;
+
+    public GameObject WinnerLabel;
+
+    public GameObject Button;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject[] Items = GameObject.FindGameObjectsWithTag("Item");
+        int countItems = Items.Length;
+        Text score_text = ScoreLabel.GetComponent<Text>();
+        score_text.text = countItems.ToString();
+
+        if (countItems == 0)
+        {
+            WinnerLabel.SetActive(true);
+            Button.SetActive(true);
+        }
+    }
+}
